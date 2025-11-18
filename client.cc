efb70d25e10df55ef6a5ae5d68036c77b49adc56
philippark <philippark271@gmail.com>
2025-11-17 22:11:46 -0500

    reflect client for a key-value server

--- client.cc
+++ client.cc
@@ -11,6 +11,9 @@
 #include <vector>
 #include <string>
 
+// TODO: REMOVE THIS
+#include <iostream>
+
 static void msg(const char *msg) {
     fprintf(stderr, "%s\n", msg);
 }
@@ -23,7 +26,7 @@ static void die(const char *msg) {
 
 // Reads n bytes from file descriptor fd
 // Returns 0 on success, -1 on error 
-static int32_t read_full(int fd, uint8_t *buf, size_t n) {
+static int32_t read_full(int fd, char *buf, size_t n) {
     while (n > 0) {
         ssize_t rv { read(fd, buf, n) };
         if (rv <= 0) {
@@ -38,7 +41,7 @@ static int32_t read_full(int fd, uint8_t *buf, size_t n) {
 
 // Writes n bytes to file descriptor fd
 // Returns 0 on success, -1 on error
-static int32_t write_all(int fd, const uint8_t *buf, size_t n) {
+static int32_t write_all(int fd, const char *buf, size_t n) {
     while (n > 0) {
         ssize_t rv { write(fd, buf, n) };
         if (rv <= 0) {
@@ -57,26 +60,40 @@ static void buf_append(std::vector<uint8_t> &buf, const uint8_t *data, size_t le
 }
 
 // maximum message payload size
-const size_t k_max_msg = 32 << 20;
+const size_t k_max_msg = 4096;
 
 // Sends a length-prefixed request to the server
 // Returns 0 on success, -1 on error
-static int32_t send_req(int fd, const uint8_t *text, size_t len) {
+static int32_t send_req(int fd, const std::vector<std::string> &cmd) {
+    uint32_t len { 4 };
+    for (const std::string &s : cmd) {
+        len += 4 + s.size();
+    }
+
     if (len > k_max_msg) {
         return -1;
     }
 
-    std::vector<uint8_t> wbuf;
-    buf_append(wbuf, reinterpret_cast<const uint8_t *>(&len), 4);
-    buf_append(wbuf, text, len);
-    return write_all(fd, wbuf.data(), wbuf.size());
+    char wbuf[4 + k_max_msg];
+    memcpy(&wbuf[0], &len, 4);
+    uint32_t n = cmd.size();
+    memcpy(&wbuf[4], &n, 4);
+    size_t cur = 8;
+    
+    for (const std::string &s : cmd) {
+        uint32_t p = static_cast<uint32_t>(s.size());
+        memcpy(&wbuf[cur], &p, 4);
+        memcpy(&wbuf[cur+4], s.data(), s.size());
+        cur += 4 + s.size();
+    }
+
+    return write_all(fd, wbuf, 4 + len);
 }
 
 // Reads a response from the server
 // Returns 0 on success, -1 on error
 static int32_t read_res(int fd) {   
-    std::vector<uint8_t> rbuf;
-    rbuf.resize(4);
+    char rbuf[4 + k_max_msg];
     errno = 0;
     
     // read in the length prefix
@@ -92,14 +109,13 @@ static int32_t read_res(int fd) {
     }
 
     uint32_t len { 0 };
-    memcpy(&len, rbuf.data(), 4);
+    memcpy(&len, rbuf, 4);
     if (len > k_max_msg) {
         msg("too long");
         return -1;
     }
 
     // read in the response
-    rbuf.resize(4 + len);  
     err = read_full(fd, &rbuf[4], len);
     if (err) {
         msg("read() error");
@@ -107,11 +123,18 @@ static int32_t read_res(int fd) {
     }
 
     // debugging purposes
-    printf("len:%u data:%.*s\n", len, len < 100 ? len : 100, &rbuf[4]);
+    uint32_t rescode = 0;
+    if (len < 4) {
+        msg("bad response");
+        return -1;
+    }
+    memcpy(&rescode, &rbuf[4], 4);
+    printf("server says: [%u] %.*s\n", rescode, len - 4, &rbuf[8]);
+
     return 0;
 }
 
-int main() {
+int main(int argc, char** argv) {
     // create listening socket
     int fd { socket(AF_INET, SOCK_STREAM, 0) };
     if (fd < 0) {
@@ -128,24 +151,19 @@ int main() {
         die("connect");
     }
 
-    // multiple pipelined requests
-    std::vector<std::string> query_list { "hello1", "hello2", "hello3",
-        std::string(k_max_msg, 'z'), // requires multiple iterations
-        "hello5",
-    };
-
-    for (auto s : query_list) {
-        int32_t err { send_req(fd, reinterpret_cast<uint8_t*>(s.data()), s.size()) };
-        if (err) {
-            goto L_DONE;
-        }
+    std::vector<std::string> cmd;
+    for (int i = 1; i < argc; ++i) {
+        cmd.push_back(argv[i]);
     }
 
-    for (size_t i = 0;  i < query_list.size(); ++i) {
-        int32_t err { read_res(fd) };
-        if (err) {
-            goto L_DONE;
-        }
+    int32_t err = send_req(fd, cmd);
+
+    if (err) {
+        goto L_DONE;
+    }
+    err = read_res(fd);
+    if (err) {
+        goto L_DONE;
     }
 
 L_DONE:
